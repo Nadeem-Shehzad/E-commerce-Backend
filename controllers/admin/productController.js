cd5df1777604ca40faffdbf93b922fea00cb786f
Nadeem-Shehzad <nadeem.dev@outlook.com>
2024-12-12 22:32:28 +0500

    add getall products and add one

--- controllers/admin/productController.js
+++ controllers/admin/productController.js
@@ -1,55 +1,65 @@
-
-
+const asyncHandler = require('express-async-handler');
+const Product = require('../../models/productModel');
 
 //@desc Get all products
 //@route GET /api/admin/product
 //@access Public
-const getAllProducts = (req, res) => {
-    res.status(200).json({ message: `Admin Get all products` });
-};
+const getAllProducts = asyncHandler(async (req, res) => {
+    const allProducts = await Product.find({});
+    res.status(200).json(allProducts);
+});
 
 
 //@desc Get single product
 //@route GET /api/admin/product/:id
 //@access Public
-const getSingleProduct = (req, res) => {
+const getSingleProduct = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Admin Single Product` });
-};
+});
 
 
 //@desc search product
 //@route GET /api/admin/product/search
 //@access Public
-const searchProduct = (req, res) => {
+const searchProduct = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Admin Search Product` });
-};
+});
 
 
 //@desc Add products
 //@route POST /api/admin/product
 //@access Public
-const addProduct = (req, res) => {
-    res.status(200).json({ message: `Add product` });
-};
+const addProduct = asyncHandler(async (req, res) => {
+    const { name, price, quantity } = req.body;
+    if (!name || !price || !quantity) {
+        res.status(400);
+        throw new Error('All Fields are mendatory !');
+    }
+
+    const product = await Product.create({
+        name,
+        price,
+        quantity
+    });
+
+    res.status(201).json(product);
+});
 
 
 //@desc Update Product
 //@route PUT /api/admin/product/:id
 //@access Public
-const updateProduct = (req, res) => {
+const updateProduct = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Update Product` });
-};
+});
 
 
 //@desc Delete product
 //@route DELETE /api/admin/product/:id
 //@access Public
-const deleteProduct = (req, res) => {
+const deleteProduct = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Delete Product` });
-};
-
-
-
+});
 
 
 
