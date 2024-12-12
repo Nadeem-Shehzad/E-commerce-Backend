326349845ba4c2362749be2a50d611c4f1cd6bef
Nadeem-Shehzad <nadeem.dev@outlook.com>
2024-12-12 16:44:13 +0500

    add admin routing

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 require('dotenv').config();
 
+const adminRoutes = require('./routes/admin/aRoutes');
 const userRoutes = require('./routes/user/uRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use('/api/user',userRoutes);
+app.use('/api/admin', adminRoutes);
+app.use('/api/user', userRoutes);
 
 
 app.listen(PORT, () => {
