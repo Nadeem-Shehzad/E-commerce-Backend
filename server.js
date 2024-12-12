552292e11cd4296c395524214c3418dd6a3ca40f
Nadeem-Shehzad <nadeem.dev@outlook.com>
2024-12-12 15:32:23 +0500

    add user and admin route

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 require('dotenv').config();
 
+const userRoutes = require('./routes/user/uRoutes');
+
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
+
+app.use('/api/user',userRoutes);
 
 
-app.get('/', (req, res) => {
-    res.status(200).json({ message: `Welcome in E-com Apis` });
-});
- 
 app.listen(PORT, () => {
     console.log(`Server is running on PORT : ${PORT}`);
 });
