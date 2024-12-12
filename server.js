65321e8bfa09b6880ec75391f72cf5f3b3d810e2
Nadeem-Shehzad <nadeem.dev@outlook.com>
2024-12-12 22:10:31 +0500

    add middlewares for error and json parsing

--- server.js
+++ server.js
@@ -3,13 +3,18 @@ require('dotenv').config();
 
 const adminRoutes = require('./routes/admin/aRoutes');
 const userRoutes = require('./routes/user/uRoutes');
+const customErrorHandler = require('./middlewares/errorHandler');
+const connectDB = require('./config/dbConnection');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+connectDB();
+
+app.use(express.json());
 app.use('/api/admin', adminRoutes);
 app.use('/api/user', userRoutes);
-
+app.use(customErrorHandler);
 
 app.listen(PORT, () => {
     console.log(`Server is running on PORT : ${PORT}`);
