3f2db46ab63342afde10607cc71a1343862178a4
Nadeem-Shehzad <nadeem.dev@outlook.com>
2025-01-01 01:20:49 +0500

    added search product api

--- routes/cProductRoutes.js
+++ routes/cProductRoutes.js
@@ -8,17 +8,18 @@ const {
     searchProduct
 } = require('../controllers/cProductController');
 
+const tokenValidator = require('../middlewares/tokenValidator');
 
 router.route('/')
     .get(getAllProducts);
 
 
-router.route('/search')
-    .get(searchProduct);
+router.route('/search-product')
+    .get(tokenValidator, searchProduct);
 
 
-router.route('/:id')
-    .get(getSingleProduct);
+router.route('/:_id')
+    .get(tokenValidator, getSingleProduct);
 
 
 module.exports = router;
